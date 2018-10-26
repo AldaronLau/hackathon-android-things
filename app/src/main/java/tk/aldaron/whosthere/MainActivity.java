9c4fffe58fed4cdb4d9be471e510baaaff895644
wowbob396 <wowbob396@gmail.com>
2018-10-25 20:19:20 -0500

    successfully captured, and displayed image

--- app/src/main/java/tk/aldaron/whosthere/MainActivity.java
+++ app/src/main/java/tk/aldaron/whosthere/MainActivity.java
@@ -2,10 +2,15 @@ package tk.aldaron.whosthere;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Skeleton of an Android Things activity.
@@ -34,13 +39,34 @@ public class MainActivity extends Activity {
     private static final int PREVIEW_IMAGE_WIDTH = 640;
     private static final int PREVIEW_IMAGE_HEIGHT = 480;
 
+    /** Image dimensions required by TF model */
+    private static final int TF_INPUT_IMAGE_WIDTH = 224;
+    private static final int TF_INPUT_IMAGE_HEIGHT = 224;
+    /** Dimensions of model inputs. */
+    private static final int DIM_BATCH_SIZE = 1;
+    private static final int DIM_PIXEL_SIZE = 3;
+
     private CameraHandler mCameraHandler;
     private Button mCameraButton;
+    private ImagePreprocessor mImagePreprocessor;
+    private ImageView mImage;
     /**
      * Initialize the camera
      */
     private void initCamera() {
+        mImagePreprocessor = new ImagePreprocessor(
+                PREVIEW_IMAGE_WIDTH, PREVIEW_IMAGE_HEIGHT,
+                TF_INPUT_IMAGE_WIDTH, TF_INPUT_IMAGE_HEIGHT);
         mCameraHandler = CameraHandler.getInstance();
+        mCameraHandler.initializeCamera(this,
+                PREVIEW_IMAGE_WIDTH, PREVIEW_IMAGE_HEIGHT, null,
+                new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader imageReader) {
+                        Bitmap bitmap = mImagePreprocessor.preprocessImage(imageReader.acquireNextImage());
+                        onPhotoReady(bitmap);
+                    }
+                });
     }
 
     /**
@@ -48,6 +74,13 @@ public class MainActivity extends Activity {
      */
     private void closeCamera() {
         mCameraHandler.shutDown();
+
+    }
+    /**
+     * Image capture process complete
+     */
+    private void onPhotoReady(Bitmap bitmap) {
+        mImage.setImageBitmap(bitmap);
     }
 
     /**
@@ -55,26 +88,26 @@ public class MainActivity extends Activity {
      */
     private void loadPhoto() {
         mCameraHandler.takePicture();
-        Bitmap bitmap = mCameraHandler.
-        onPhotoReady();
-
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mCameraButton = findViewById(R.id.button);
-
+        mImage = findViewById(R.id.camera);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        initCamera();
         mCameraButton.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 mCameraHandler.takePicture();
+                 loadPhoto();
              }
          }
 
         );
-        initCamera();
+
     }
 
 }
